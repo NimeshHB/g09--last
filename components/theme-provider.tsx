33c56f07c6345203ee64a27942094d245db3581b
laksha413 <lakshanmaduwanthakatuwanaarach@gmail.com>
2025-09-28 18:22:04 +0530

    Update theme-provider11.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,24 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
+/**
+ * A small wrapper around next-themes' ThemeProvider that:
+ * - provides safe defaults (attribute and defaultTheme)
+ * - is memoized to avoid unnecessary re-renders
+ */
+export const ThemeProvider: React.FC<ThemeProviderProps> = React.memo(
+  function ThemeProvider({
+    children,
+    attribute = 'class',     // use `class` attribute by default (commonly used with tailwind)
+    defaultTheme = 'system',  // follow system preference by default
+    ...props
+  }: ThemeProviderProps) {
+    return (
+      <NextThemesProvider attribute={attribute} defaultTheme={defaultTheme} {...props}>
+        {children}
+      </NextThemesProvider>
+    )
+  }
+)
+
+ThemeProvider.displayName = 'ThemeProvider'
